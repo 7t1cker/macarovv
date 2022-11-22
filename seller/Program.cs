a1f19aeb7ee21f518687d990ba92bd7f49f092ae
7t1cker <vadimbarkalov367@gmail.com>
2022-11-22 10:32:59 +0300

    vou

--- seller/Program.cs
+++ seller/Program.cs
@@ -5,11 +5,9 @@ namespace clown
     {
         static void Main()
         {
-            string []  target = new string[] { "crm.ozon.ru//profile", "https://confluence.o3.ru/pages/viewpage.action?pageId=216024852", "", "https://seller-edu.ozon.ru/" };
-            for (int i = 0; i < target.Length; i++)
-            {
-                System.Diagnostics.Process.Start(target[i]); System.Threading.Thread.Sleep(500);
-            }
+            string kil = "";
+            kil= kil.ToUpper();
+            Console.WriteLine("1/ 1/");
         }
     }
 }
