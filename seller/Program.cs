316a9da6f67c3d3104bbba672f23e902a9905ff9
7t1cker <vadimbarkalov367@gmail.com>
2022-11-22 10:19:05 +0300

    xoooo

---
+++ seller/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace clown
+{
+    class popik
+    {
+        static void Main()
+        {
+            string []  target = new string[] { "crm.ozon.ru//profile", "https://confluence.o3.ru/pages/viewpage.action?pageId=216024852", "", "https://seller-edu.ozon.ru/" };
+            for (int i = 0; i < target.Length; i++)
+            {
+                System.Diagnostics.Process.Start(target[i]); System.Threading.Thread.Sleep(500);
+            }
+        }
+    }
+}
